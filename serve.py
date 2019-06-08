3b602d167c3b7b187eb78f88b8decc9e25e82597
Nick Chadwick <chadnickbok@gmail.com>
2019-06-08 14:56:23 -0700

    Initial sketch of broadcast demo

---
+++ serve.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from flask import Flask, send_from_directory
+from flask_sockets import Sockets
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+clients = []
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    print("Hey cool a client")
+    clients.append(ws)
+    while not ws.closed:
+        message = ws.receive()
+        for client in clients:
+            if client is not ws:
+                try:
+                    client.send(message)
+                except:
+                    print("Found a dead client")
+
+    clients.remove(ws)
+
+@app.route('/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
+
+if __name__ == "__main__":
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
